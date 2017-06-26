9f718ef129c2e69a31d0b6e9a27c960c7ad59ca4
Pranav <pranav.ppanch@gmail.com>
2017-06-26 14:46:40 +0530

    Matrix back propagation

--- code.py
+++ code.py
@@ -1,8 +1,33 @@
-#import numpy as np
-class layer:
-  m = numpy.matrix([[1,2,3],[1,2,3]])
-  def disp(self):
-    print(m)
+import numpy as np
+import random
 
-l = layer()
-disp(l)
+stepsize= 1 
+
+class layer:
+  def __init__(self,x,y):
+    self.x = x
+    self.y = y
+    self.m = np.random.random((y,x))
+
+  def disp(self):
+    print(self.m)
+
+  def update(self,val,chain):
+    chain = sigmaprime(val,chain)  
+    #Matrix Back Propagation
+    for i in range(self.y):
+      for j in range(self.x):
+        self.m[i][j] -= stepsize*self.m[i][j]*chain[i]
+    
+#Class Definition Ends  
+
+def sigmaprime(x,chain):
+  return 1*chain;
+
+def sigma(x):
+  return x
+
+l = layer(2,3)
+l.disp()
+l.update([1,1,1],[1,1,1])
+l.disp()
